98e2e3828b60480c4374dbb6460fd1f8cbf200f9
snnyzi <carcelesfranck@gmail.com>
2025-12-04 20:10:02 +0100

    Structure un peu plus globale du site
    architecture divisé en header body et footer mais adapté en html4

--- index.php
+++ index.php
@@ -9,13 +9,40 @@ Ceci se passe de commentaire.
  <meta http-equiv="Content-Language" content="fr" />
  <meta name="author" content="Marc Glisse" />
  <title>
-  Squelette
+  SlamBlog
  </title>
+ <div>
+    Présentation
+ </div>
+  <div>
+    Que faisons nous ?
+ </div>
+  <div>
+    FAQ
+ </div>
+ <hr />
+
 </head>
 <body>
 <p>
-Allez dans <code>Fichier&rarr;Enregistrer sous</code>, pour t&eacute;l&eacute;charger ce fichier.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in velit risus. Ut lobortis mollis risus, sed bibendum eros pellentesque in. Aliquam at varius tellus. Pellentesque aliquam urna ut lorem malesuada, et pellentesque ligula feugiat. Morbi egestas nisl sit amet leo vehicula, eu finibus risus molestie. Suspendisse potenti. Integer mi justo, lobortis et enim in, congue egestas orci. Ut at risus ultricies, vulputate mauris sed, rhoncus ipsum. Phasellus interdum dolor in ligula mattis, non aliquet lorem vestibulum. Aliquam ultricies cursus turpis non eleifend. In imperdiet fermentum porttitor.
+</p>
+<p>
+Aenean dictum nulla in tempus venenatis. Praesent at ultricies lacus, id condimentum leo. In finibus dignissim risus, vel vestibulum orci fringilla non. Aliquam nunc leo, suscipit id diam at, auctor pretium diam. Vestibulum odio nunc, congue vitae pretium non, placerat eget nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis ipsum mauris. Aliquam aliquam scelerisque leo, at suscipit urna pharetra id. Phasellus commodo lacus non placerat tempus. Ut varius est sit amet viverra commodo. Cras in leo condimentum, venenatis massa quis, rhoncus quam. Ut tincidunt ut nisl mattis aliquam.
+</p>
+<p>
+Pellentesque ornare placerat venenatis. Sed venenatis ante nec enim fringilla sagittis. Suspendisse eleifend viverra consequat. Suspendisse eu nisl est. Quisque ultrices elit sem, non faucibus orci condimentum eu. Morbi volutpat efficitur consectetur. Nullam faucibus porttitor nibh vel feugiat. Vestibulum massa lacus, dignissim id enim et, tempor sodales leo. Cras porta non ante eget molestie. Phasellus a iaculis elit. Donec iaculis lectus eu enim pellentesque, sit amet tristique lorem iaculis. Nulla orci mauris, vestibulum eget urna eget, pulvinar imperdiet est. Pellentesque hendrerit purus a est ultricies, quis pretium libero auctor. Praesent semper nisi in mi dignissim rhoncus.
+</p>
+<p>
+Sed tempus in libero at convallis. Fusce eget massa at neque laoreet vulputate. Praesent nunc est, placerat eget libero id, condimentum faucibus lorem. Vestibulum quis tellus ipsum. Vivamus sed ornare turpis. Cras hendrerit urna at est volutpat scelerisque. Proin quis odio elit. Aenean ipsum lectus, viverra at aliquam nec, ullamcorper ut augue.
+</p>
+<p>
+Pellentesque id ligula vitae nulla vehicula scelerisque. Etiam magna libero, feugiat vitae nulla in, faucibus euismod odio. Nam ullamcorper non turpis at cursus. Fusce commodo, felis vel pulvinar pulvinar, risus purus mattis nisl, et venenatis erat lacus nec dolor. Integer sit amet fermentum eros. Aliquam tincidunt, arcu sed feugiat pellentesque, urna orci faucibus purus, at lobortis velit est et ante. Pellentesque eget neque eu felis interdum tincidunt ut ac ipsum. Vestibulum id nibh aliquet, dictum mi nec, malesuada elit. Donec non pulvinar ante, non imperdiet risus. Sed malesuada rhoncus vestibulum. Ut eleifend leo orci, sit amet ornare massa ultricies ut. Nunc nec eros eget libero scelerisque posuere ac sit amet ante. Vivamus enim turpis, ultrices quis leo at, blandit placerat risus. Morbi congue dui quis lorem molestie congue. Cras iaculis nulla nec tellus semper accumsan. Nunc laoreet ullamcorper vulputate.
 </p>
 <hr />
+
+<p>
+footer
+</p>
 </body>
 </html>
